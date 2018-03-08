cd6590055e62141603ca3650bec292e003986110
Mahesh S <37013197+Mahesh2877@users.noreply.github.com>
2018-03-08 16:31:29 +0530

    Update code.c

--- code.c
+++ code.c
@@ -1,6 +1,32 @@
-#include<stdio.h>
-void main()
+#include<iostream>
+class Quadratic
   {
-    printf("Hello World\n");
-  }
-  getch();
+    int a,b,c;
+    public: x1,x2;
+            void input()
+      {
+        cin>>x>>y>>z;
+        a=x;
+        b=y;
+        c=z;
+      }
+       void calculate()
+       	{
+          x1 = (-b - sqrt((b.b) - (4.a.c)))/(2.a.c)
+          x2 = (-b + sqrt((b.b) - (4.a.c)))/(2.a.c)
+        }  
+       void print()
+			 	{
+			 		cout<<"The Solutions to the above Quadratic Equation are:"<<endl;
+					cout<<x1<<" and "<<x2<<endl;
+				}
+			}
+int main()
+{
+  Quadratic obj;
+  Cout<<"Enter the values of a, b and c"<<endl;
+	obj.input();
+	obj.calculate();
+	obj.print();
+	return 0;
+}
